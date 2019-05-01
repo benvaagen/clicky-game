14d3745b396c6319d2c1ca3a485ba25d73c882c0
bvaagen <benvaagen24@gmail.com>
2019-05-01 15:18:52 -0700

    Updates

---
+++ precache-manifest.b0899a5218e70839886178dc932765f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d40042bbbd8b68c8b6a80ac0d31d46",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "72bcbf2e157c0bfca7bc",
+    "url": "/clicky-game/static/css/main.5d790f5a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "72bcbf2e157c0bfca7bc",
+    "url": "/clicky-game/static/js/main.31ed2f11.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
